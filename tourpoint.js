8eb6f9c8bdedc9321271b929f601afd66f0a8609
arithatx <124546062+arithatx@users.noreply.github.com>
2023-05-29 11:05:30 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,157 @@
+var tourpoint = 
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "บอนชอน",
+          "type": "food",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.53207218650545,
+            13.745736570412546
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "KFC",
+          "type": "food",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.53022426671794,
+            13.744063319851847
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ส้มตำคุณกัญจณ์",
+          "type": "food",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.62129269618771,
+            13.685223100488287
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ครัวทับเเก้วหมูจุ่ม",
+          "type": "food",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.62410669341557,
+            13.684706660648231
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เจริญรสชาบู",
+          "type": "food",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.53986697395396,
+            13.769151830136721
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "level": 10,
+          "name": "ทองสมิทธิ์",
+          "type": "food"
+        },
+        "geometry": {
+          "coordinates": [
+            100.53479391534592,
+            13.746376320165368
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สุกี้ตี๋น้อย",
+          "type": "food",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.57892927116438,
+            13.784458305459182
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "MK",
+          "type": "food",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.56975139021455,
+            13.766820391902925
+          ],
+          "type": "Point"
+        },
+        "id": 8
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "burgerking",
+          "type": "food",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.572138565626,
+            13.770137722276985
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เล็กต้มยำ",
+          "type": "food",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.50771608465777,
+            13.717978599498934
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
